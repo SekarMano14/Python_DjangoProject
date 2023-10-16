4dec0ee13e85c4835bedf7d7b78fce5507008c70
SekarMano14 <sekarmano46@gmail.com>
2023-10-16 20:52:06 +0530

    My project

--- MyDjangoProject/MyDjangoProject/settings.py
+++ MyDjangoProject/MyDjangoProject/settings.py
@@ -69,7 +69,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [BASE_DIR.joinpath("templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
